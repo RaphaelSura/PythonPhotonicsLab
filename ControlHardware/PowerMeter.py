251b155ef0a58c18bc41b911de1aadf8b3fee955
RaphaelSura <daveau.raphael@gmail.com>
2019-07-26 15:56:37 -0400

    Commit July 2019

--- ControlHardware/PowerMeter.py
+++ ControlHardware/PowerMeter.py
@@ -9,8 +9,16 @@ samp_per_reading = 20
 
 """  calibration values (532nm), power read at the output of microscope objective
      ND 1 in front of photo diode, , cannot read higher than 3 uW  """
+
 power_reading = [0.1, 14.3, 29.5, 47.5, 228, 497, 1005, 1960, 2460, 3340]
 daq_reading = [0.019, 0.065, 0.11, 0.155, 0.67, 1.44, 2.89, 5.67, 6.98, 9.53]
+
+
+# Alternative calibration
+"""
+power_reading = [10, 96, 1170, 14700, 4500, 5570, 7360]
+daq_reading = [0.018, .022, .089, .939, .299, .366, .477]
+"""
 
 
 class PowerMeterApp:
@@ -29,6 +37,14 @@ class PowerMeterApp:
         self.nw_label = Label(self.win, text=" nW ", font=app_font)
         self.nw_label.grid(row=0, column=1, padx=10, pady=10)
 
+        # Display DAQ reading
+        self.v2 = StringVar()
+        self.v2.set("0.00")
+        """
+        self.nw_label = Label(self.win, textvariable=self.v2, width=8, font=app_font)
+        self.nw_label.grid(row=1, column=0, padx=10, pady=10)
+        """
+
         #initialize the task on DAQ board
         self.task = nidaqmx.Task("PD reading")
         self.task.ai_channels.add_ai_voltage_chan("Dev1/ai2")
@@ -38,6 +54,7 @@ class PowerMeterApp:
             v_now = self.read_voltage()
             current_power = self.volt_to_power(np.mean(v_now))
             self.v.set(str(np.round(current_power, 3)))
+            self.v2.set(str(np.round(v_now, 3)))
             self.win.update()
 
     def read_voltage(self):
